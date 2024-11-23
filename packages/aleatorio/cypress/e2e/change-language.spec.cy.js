f036f2930c320b49cb9072be9423837f81d05483
gorozcoua <g.orozcos@uniandes.edu.co>
2024-11-22 20:58:04 -0500

    E00501 faker

--- packages/aleatorio/cypress/e2e/change-language.spec.cy.js
+++ packages/aleatorio/cypress/e2e/change-language.spec.cy.js
@@ -1,8 +1,13 @@
+// Import faker
+const { faker } = require('@faker-js/faker');
+
 describe("F004 - Configurar lenguaje de publicación", () => {
-    it("E00501 - Modificar lenguaje de ghost", () => {
- 
+    it("E00501 - Modificar lenguaje de Ghost", () => {
+        // Generate dynamic data (example)
+        const randomLanguage = faker.helpers.arrayElement(["es", "en", "fr", "de", "it"]); 
+
         cy.log('Given I am an admin logged in with email "<ADMIN_USERNAME>" and password "<ADMIN_PASSWORD>"');
-        cy.loginPage.loginAs(Cypress.env("ADMIN_USERNAME"),Cypress.env("ADMIN_PASSWORD"));
+        cy.loginPage.loginAs(Cypress.env("ADMIN_USERNAME"), Cypress.env("ADMIN_PASSWORD"));
 
         cy.log('And I click in admin setting');
         cy.changeLanguage.clickAdminSetting();
@@ -10,46 +15,17 @@ describe("F004 - Configurar lenguaje de publicación", () => {
         cy.log('And I click in edit language');
         cy.changeLanguage.clickEditLanguage();
 
-        cy.log('And I edit language "es"');
-        cy.changeLanguage.editLanguage("es");
+        cy.log(`And I edit language "${randomLanguage}"`);
+        cy.changeLanguage.editLanguage(randomLanguage);
 
         cy.log('When I click in save language');
         cy.changeLanguage.saveLanguage();
 
-        cy.log('And I navegate to home page');
+        cy.log('And I navigate to home page');
         cy.changeLanguage.navigateToHomePage();
 
         // Then
-        cy.log('I verify the language "es"');
-        cy.changeLanguage.verifyLanguage("es");
+        cy.log(`I verify the language "${randomLanguage}"`);
+        cy.changeLanguage.verifyLanguage(randomLanguage);
     });
-
-
-
-    it("E00502 - Modificar lenguaje con espacios en blanco", () => {
- 
-        cy.log('Given I am an admin logged in with email "<ADMIN_USERNAME>" and password "<ADMIN_PASSWORD>"');
-        cy.loginPage.loginAs(Cypress.env("ADMIN_USERNAME"),Cypress.env("ADMIN_PASSWORD"));
-
-        cy.log('And I click in admin setting');
-        cy.changeLanguage.clickAdminSetting();
-
-        cy.log('And I click in edit language');
-        cy.changeLanguage.clickEditLanguage();
-
-        cy.log('And I edit language "es   "');
-        cy.changeLanguage.editLanguage("es   ");
-
-        cy.log('When I click in save language');
-        cy.changeLanguage.saveLanguage();
-
-        cy.log('And I navegate to home page');
-        cy.changeLanguage.navigateToHomePage();
-
-        // Then
-        cy.log('I verify the language "es   "');
-        cy.changeLanguage.verifyLanguage("es   ");
-    });
-
-
 });
