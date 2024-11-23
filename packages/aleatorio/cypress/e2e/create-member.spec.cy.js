53a9d35fadf4138326d98a447a02685017fd3dda
gorozcoua <g.orozcos@uniandes.edu.co>
2024-11-23 16:06:07 -0500

    E00803 - should log in and create a new member with a long name successfully

--- packages/aleatorio/cypress/e2e/create-member.spec.cy.js
+++ packages/aleatorio/cypress/e2e/create-member.spec.cy.js
@@ -70,4 +70,38 @@ describe("F009 - Ghost Admin Login and Member Creation", () => {
         cy.log("Then an error should be displayed due to missing email");
         cy.membersPage.verifyEmailError();
     });
+
+    it("E00803 - should log in and create a new member with a long name successfully", () => {
+        cy.log(
+            'Given I am an admin logged in with email "<ADMIN_USERNAME>" and password "<ADMIN_PASSWORD>"'
+        );
+        cy.loginPage.loginAs(
+            Cypress.env("ADMIN_USERNAME"),
+            Cypress.env("ADMIN_PASSWORD")
+        );
+    
+        cy.log("Then the user should be redirected to the dashboard");
+        cy.wait(5000);
+        cy.dashboardPage.verifyDashboard();
+    
+        cy.log("Given the user navigates to the Members section");
+        cy.dashboardPage.navigateToMembers();
+    
+        cy.log("When the user opens the new member form");
+        cy.membersPage.openNewMemberForm();
+        cy.log("Then the new member form should be displayed");
+    
+        const longMemberName = faker.lorem.words(15); // Generate a very long name
+        const memberEmail = faker.internet.email();
+    
+        cy.log("Given the user has entered the new member details with a long name");
+        cy.membersPage.fillMemberDetailsComplete(longMemberName, memberEmail);
+    
+        cy.log("When the user saves the new member");
+        cy.membersPage.saveMember();
+    
+        cy.log("Then the new member should be created successfully");
+        cy.membersPage.verifyMemberCreation();
+    });
+    
 });
