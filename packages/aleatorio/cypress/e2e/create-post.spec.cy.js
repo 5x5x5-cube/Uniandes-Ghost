543a57157f6d1ca617c86d6ea8d2b5bcb67ab82a
gorozcoua <g.orozcos@uniandes.edu.co>
2024-11-23 16:16:19 -0500

    E00203 - Crear un post con caracteres especiales en el título

--- packages/aleatorio/cypress/e2e/create-post.spec.cy.js
+++ packages/aleatorio/cypress/e2e/create-post.spec.cy.js
@@ -67,4 +67,35 @@ describe("F002 - Crear post", () => {
         cy.log("And I wait for 2 seconds");
         cy.wait(2000);
     });
+
+    it("E00203 - Crear un post con caracteres especiales en el título", () => {
+        const postTitle = faker.helpers.replaceSymbols("Título !@#$%^&*()?");
+        const postUrl = faker.word.words(1);
+    
+        // Given
+        cy.log(
+            `Given I am an admin logged in with email "${adminUsername}" and password "${adminPassword}"`
+        );
+        cy.loginPage.loginAs(adminUsername, adminPassword);
+    
+        cy.log("And I am on the post editor page");
+        cy.postEditorPage.visit();
+    
+        // When
+        cy.log(
+            `When I create and publish a post with title "${postTitle}" and url "${postUrl}"`
+        );
+        cy.postEditorPage.createAndPublishPost(postTitle, postUrl);
+    
+        cy.log(`And I navigate to the post url "${postUrl}"`);
+        cy.postViewerPage.navigateToPost(postUrl);
+    
+        // Then
+        cy.log(`Then I should see a page with the post title "${postTitle}"`);
+        cy.postViewerPage.getPostTitle().should("have.text", postTitle);
+    
+        cy.log("And I wait for 2 seconds");
+        cy.wait(2000);
+    });
+    
 });
