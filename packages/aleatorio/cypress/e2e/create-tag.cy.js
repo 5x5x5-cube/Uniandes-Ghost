65f7902c3c985d0b47fa501b7ca7e9e19d62c549
gorozcoua <g.orozcos@uniandes.edu.co>
2024-11-23 16:19:09 -0500

    E01003 - Crear una etiqueta con caracteres especiales

--- packages/aleatorio/cypress/e2e/create-tag.cy.js
+++ packages/aleatorio/cypress/e2e/create-tag.cy.js
@@ -59,4 +59,32 @@ describe("F010 - Crear tag", () => {
         cy.log("And I wait for 2 seconds");
         cy.wait(2000);
     });
+
+    it("E01003 - Crear una etiqueta con caracteres especiales", () => {
+        const tagName = faker.helpers.replaceSymbols("Etiqueta !@#$%^&*()?");
+    
+        // Given
+        cy.log(
+            `Given I am an admin logged in with email "${adminUsername}" and password "${adminPassword}"`
+        );
+        cy.loginPage.loginAs(adminUsername, adminPassword);
+    
+        cy.log("And I am in the Tag editor page");
+        cy.tagEditorPage.navigateToTagEditorPage();
+    
+        // When
+        cy.log(`When I create a new tag "${tagName}"`);
+        cy.tagEditorPage.createTag(tagName);
+    
+        cy.log("And I navigate to the Tag list page");
+        cy.tagListPage.navigateToTagListPage();
+    
+        // Then
+        cy.log(`Then I should see the new tag name "${tagName}"`);
+        cy.tagListPage.getTagFromList(tagName).should("exist");
+    
+        cy.log("And I wait for 2 seconds");
+        cy.wait(2000);
+    });
+    
 });
